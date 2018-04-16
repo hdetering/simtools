47d11ed22f03de56954625044da32272df902873
Harald Detering <harald.detering@gmail.com>
2018-04-16 11:31:25 +0200

    Added setup.py

---
+++ simtools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simtools',
+      version='0.1',
+      description='Useful tools in the context of tumor data simulations.',
+      url='http://github.com/hdetering/simtools',
+      author='Harald Detering',
+      author_email='harald.detering@gmail.com',
+      license='GPL-3.0',
+      packages=['simtools'],
+      zip_safe=False)
